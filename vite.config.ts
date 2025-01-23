f070a9e70ba3ffaae0ffba7c85e08f2a909297c7
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2025-01-22 23:36:00 -0800

    add enhanced image correctly

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig, type Plugin } from 'vite';
 
 export default defineConfig({
 	// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
-	plugins: [tailwindcss() as Plugin[], sveltekit()]
+	plugins: [enhancedImages(), tailwindcss() as Plugin[], sveltekit()]
 });
