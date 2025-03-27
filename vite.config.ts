cc66c9035341f3622e2006572290cde4e35e6c4a
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2025-03-26 22:47:51 -0700

    pin and update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig, type Plugin } from 'vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-	// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
-	plugins: [enhancedImages(), tailwindcss() as Plugin[], sveltekit()]
+	plugins: [
+		enhancedImages(),
+		tailwindcss(),
+		sveltekit(),
+	],
+	build: {
+		target: 'es2023',
+	},
+	server: {
+		watch: {
+			// test files are never imported so shouldn't ever trigger a reload
+			ignored: ['**/*.test.ts', '**/*.test.js'],
+		},
+	},
 });
